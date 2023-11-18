9af242c3e60600ab3b04c0dfb521fe56eaef254c
Lucas Dimer Justo <lucas.dimer.justo@hotmail.com>
2023-11-18 14:49:44 -0300

    init xcode with iOS 16.0 target. iPhone protrait only

---
+++ letterise/letterise/letteriseApp.swift
@@ -0,0 +1,17 @@
+//
+//  letteriseApp.swift
+//  letterise
+//
+//  Created by Lucas Justo on 18/11/23.
+//
+
+import SwiftUI
+
+@main
+struct letteriseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
