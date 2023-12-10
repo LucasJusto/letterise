51cf966b9a6ca4a6834defd8450e8a0583a3e48c
Marcelo Diefenbach <marcelodiefenabch@MacBook-Pro-de-Marcelo-2.local>
2023-12-10 08:56:43 -0300

    agora pede o nickname

--- letterise/letterise/Resource/Auth/AuthSingleton.swift
+++ letterise/letterise/Resource/Auth/AuthSingleton.swift
@@ -7,93 +7,233 @@
 
 import Foundation
 
+public enum AuthStatus: Error {
+    case dontHaveInGameNickName
+    case logged
+    case error
+    case invalidURL
+    case inauthenticated
+    case authenticating
+}
+
+enum NickNameStatus: Error {
+    case nicknameInUse
+    case userNotFound
+    case serverError(String)
+    case unknownError
+    case success
+}
+
+
 class AuthSingleton: ObservableObject {
     static let shared = AuthSingleton()
-
-    private init() {
-    }
+    
+    private init() {}
     
     @Published var isLogged: Bool = false
     @Published var isAuthenticating: Bool = true
+    @Published var authenticationStatus: AuthStatus = .authenticating
+    let url = "http://gpt-treinador.herokuapp.com"
     
     var actualUser: UserModel = UserModel(id: 1, iCloudID: "iCloudID", credits: 0)
-
-    func doAuth(userId: String, completion: @escaping (Result<String, Error>) -> Void) {
-        guard let url = URL(string: "http://gpt-treinador.herokuapp.com/letterise/auth") else {
-            completion(.failure(NSError(domain: "", code: 1, userInfo: [NSLocalizedDescriptionKey: "URL inválida"])))
+    
+    func changeAuthStatus(status: AuthStatus) {
+        DispatchQueue.main.async {
+            AuthSingleton.shared.authenticationStatus = status
+        }
+    }
+    
+    func doAuth(icloudID: String, completion: @escaping (AuthStatus) -> Void) {
+        guard let url = URL(string: "\(url)/letterise/auth") else {
+            completion(.invalidURL)
             return
         }
-
+        
         var request = URLRequest(url: url)
         request.httpMethod = "POST"
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-
-        print(userId)
-        let body: [String: Any] = ["iCloudID": userId]
+        
+        print(icloudID)
+        let body: [String: Any] = ["iCloudID": icloudID]
         request.httpBody = try? JSONSerialization.data(withJSONObject: body, options: [])
-
+        
         URLSession.shared.dataTask(with: request) { data, response, error in
             if let error = error {
-                completion(.failure(error))
+                completion(.error)
                 return
             }
-
+            
             guard let data = data, let responseString = String(data: data, encoding: .utf8) else {
-                completion(.failure(NSError(domain: "", code: 2, userInfo: [NSLocalizedDescriptionKey: "Nenhum dado recebido"])))
+                // nao veio data
+                completion(.error)
                 return
             }
-            print(responseString)
-            completion(.success(responseString))
+            
+            if let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 {
+                if let jsonData = responseString.data(using: .utf8) {
+                    do {
+                        if let json = try JSONSerialization.jsonObject(with: jsonData, options: []) as? [String: Any] {
+                            if let user = json["user"] as? [String: Any] {
+                                if let credits = user["credits"] as? Int,
+                                   let id = user["id"] as? Int,
+                                   let inGameNickname = user["inGameNickname"] as? String {
+                                    print("Id: \(id)")
+                                    print("Credits: \(credits)")
+                                    print("inGameNickname: \(inGameNickname)")
+                                    AuthSingleton.shared.actualUser = UserModel(id: id, iCloudID: icloudID, credits: credits, inGameNickName: inGameNickname)
+                                    self.changeAuthStatus(status: .logged)
+                                    completion(.logged)
+                                } else {
+                                    self.changeAuthStatus(status: .error)
+                                    completion(.error)
+                                }
+                            } else {
+                                self.changeAuthStatus(status: .error)
+                                completion(.error)
+                            }
+                        } else {
+                            self.changeAuthStatus(status: .error)
+                            completion(.error)
+                        }
+                    } catch {
+                        self.changeAuthStatus(status: .error)
+                        print("Erro ao decodificar JSON: \(error)")
+                        completion(.error)
+                    }
+                }
+                completion(.error)
+                return
+            }
+            
+            if let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 400 {
+                self.changeAuthStatus(status: .dontHaveInGameNickName)
+                completion(.dontHaveInGameNickName)
+            }
+            
+            if let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 201 {
+                if let jsonData = responseString.data(using: .utf8) {
+                    do {
+                        if let json = try JSONSerialization.jsonObject(with: jsonData, options: []) as? [String: Any] {
+                            if let user = json["user"] as? [String: Any] {
+                                if let credits = user["credits"] as? Int,
+                                   let id = user["id"] as? Int {
+                                    print("Id: \(id)")
+                                    print("Credits: \(credits)")
+                                    AuthSingleton.shared.actualUser = UserModel(id: id, iCloudID: icloudID, credits: credits)
+                                    self.changeAuthStatus(status: .dontHaveInGameNickName)
+                                    completion(.dontHaveInGameNickName)
+                                } else {
+                                    self.changeAuthStatus(status: .error)
+                                    completion(.error)
+                                }
+                            } else {
+                                self.changeAuthStatus(status: .error)
+                                completion(.error)
+                            }
+                        } else {
+                            self.changeAuthStatus(status: .error)
+                            completion(.error)
+                        }
+                        self.changeAuthStatus(status: .error)
+                        completion(.error)
+                    } catch {
+                        self.changeAuthStatus(status: .error)
+                        print("Erro ao decodificar JSON: \(error)")
+                        completion(.error)
+                    }
+                }
+                //requisicao invalida
+                self.changeAuthStatus(status: .error)
+                completion(.error)
+                return
+            }
+            
+            if let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 500 {
+                self.changeAuthStatus(status: .error)
+                completion(.error)
+            }
         }.resume()
     }
     
     func checkCredentials() {
         if let userCredential = UserDefaults.standard.string(forKey: "userCredential") {
-            doAuth(userId: userCredential) { [weak self] result in
+            doAuth(icloudID: userCredential) { [weak self] result in
                 guard let self = self else { return }
                 switch result {
-                case .success(let responseString):
-                    // Convertendo a string para Data
-                    if let jsonData = responseString.data(using: .utf8) {
-                        do {
-                            // Tenta converter a resposta em um dicionário
-                            if let json = try JSONSerialization.jsonObject(with: jsonData, options: []) as? [String: Any] {
-                                // Acessa o dicionário aninhado 'user'
-                                if let user = json["user"] as? [String: Any] {
-                                    if let credits = user["credits"] as? Int,
-                                       let id = user["id"] as? Int {
-                                        print("Id: \(id)")
-                                        print("Credits: \(credits)")
-                                        AuthSingleton.shared.actualUser = UserModel(id: id, iCloudID: userCredential, credits: credits)
-                                        self.setIsLogged(bool: true)
-                                    } else {
-                                        self.setIsLogged(bool: false)
-                                    }
-                                } else {
-                                    self.setIsLogged(bool: false)
-                                }
-                            } else {
-                                self.setIsLogged(bool: false)
-                            }
-                            self.setIsAuthenticating(bool: false)
-                        } catch {
-                            print("Erro ao decodificar JSON: \(error)")
-                            self.setIsLogged(bool: false)
-                            self.setIsAuthenticating(bool: false)
-                        }
-                    }
-                    
-                    
-                case .failure(let error):
-                    print("Erro ao adicionar usuário: \(error.localizedDescription)")
-                    setIsLogged(bool: false)
-                    setIsAuthenticating(bool: false)
-  }
+                case .dontHaveInGameNickName:
+                    print("precisa cadastrar username")
+                case .logged:
+                    print("fez login")
+                case .invalidURL:
+                    print("invalid URL")
+                case .error:
+                    print("Erro na autenticacao")
+                case .inauthenticated:
+                    print("ainda não autenticou")
+                case .authenticating:
+                    print("autenticando")
+                }
             }
             
         } else {
-            setIsAuthenticating(bool: false)
+            self.changeAuthStatus(status: .inauthenticated)
         }
+    }
+    
+    func registerInGameNickname(iCloudID: String, inGameNickname: String, completion: @escaping (Result<NickNameStatus, NickNameStatus>) -> Void) {
+        guard let url = URL(string: "\(url)/letterise/register-nickname") else { return }
+
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+
+        let body: [String: String] = ["iCloudID": UserDefaults.standard.string(forKey: "userCredential") ?? "", "inGameNickname": inGameNickname]
+        request.httpBody = try? JSONSerialization.data(withJSONObject: body)
+
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            guard error == nil else {
+                completion(.failure(.unknownError))
+                return
+            }
+
+            guard let httpResponse = response as? HTTPURLResponse else {
+                completion(.failure(.unknownError))
+                return
+            }
+
+            switch httpResponse.statusCode {
+            case 200:
+                completion(.success(.success))
+            case 400:
+                guard let data = data, let responseString = String(data: data, encoding: .utf8) else {
+                    completion(.failure(.unknownError))
+                    return
+                }
+                if let jsonData = responseString.data(using: .utf8) {
+                    do {
+                        if let json = try JSONSerialization.jsonObject(with: jsonData, options: []) as? [String: Any] {
+                            if json["error"] as? String == "inGameNickname already in use." {
+                                completion(.success(.nicknameInUse))
+                            } else {
+                                completion(.failure(.unknownError))
+                            }
+                        } else {
+                            completion(.failure(.unknownError))
+                        }
+                    } catch {
+                        print("Erro ao decodificar JSON: \(error)")
+                        completion(.failure(.unknownError))
+                    }
+                }
+            case 404:
+                completion(.failure(.userNotFound))
+            case 500:
+                let errorMessage = String(data: data ?? Data(), encoding: .utf8) ?? "Unknown error"
+                completion(.failure(.serverError(errorMessage)))
+            default:
+                completion(.failure(.unknownError))
+            }
+        }.resume()
     }
     
     private func setIsAuthenticating(bool: Bool) {
@@ -117,24 +257,24 @@ class AuthSingleton: ObservableObject {
         var request = URLRequest(url: url)
         request.httpMethod = "POST"
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-
+        
         let body: [String: Any] = [
             "userID": "\(actualUser.id)",
             "credits": amount
         ]
-
+        
         request.httpBody = try? JSONSerialization.data(withJSONObject: body)
-
+        
         let task = URLSession.shared.dataTask(with: request) { data, response, error in
             guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
                 completion(false)
                 return
             }
             self.actualUser.credits = self.actualUser.credits + Int(amount)!
-
+            
             completion(true)
         }
-
+        
         task.resume()
     }
     
@@ -143,14 +283,14 @@ class AuthSingleton: ObservableObject {
         var request = URLRequest(url: url)
         request.httpMethod = "POST"
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-
+        
         let body: [String: Any] = [
             "userID": "\(actualUser.id)",
             "credits": amount
         ]
-
+        
         request.httpBody = try? JSONSerialization.data(withJSONObject: body)
-
+        
         let task = URLSession.shared.dataTask(with: request) { data, response, error in
             guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
                 completion(false)
@@ -159,7 +299,7 @@ class AuthSingleton: ObservableObject {
             self.actualUser.credits = self.actualUser.credits - Int(amount)!
             completion(true)
         }
-
+        
         task.resume()
     }
 }
---
+++ letterise/letterise/Scene/Login/SetNickName/SetNicknameView.swift
@@ -0,0 +1,116 @@
+//
+//  SetNicknameView.swift
+//  letterise
+//
+//  Created by Marcelo Diefenbach on 09/12/23.
+//
+
+import SwiftUI
+
+struct SetNicknameView: View {
+    @Environment(\.designTokens) var tokens
+    @FocusState private var isTextFieldFocused: Bool
+    @State var nickname: String = ""
+    @State var showAlert = false
+    @State var alertTitle: String = ""
+    @State var alertSubtitle: String = ""
+    @State var isLoading = false
+    
+    var body: some View {
+        ZStack {
+            VStack {
+                
+                Text("Need to choose an in game nickname")
+                    .font(.title).bold()
+                    .foregroundStyle(.black)
+                    .multilineTextAlignment(.center)
+                    .padding(.bottom, 8)
+                
+                Text("This nickname will be used to save your rankings")
+                    .font(.caption)
+                    .foregroundStyle(.gray)
+                    .multilineTextAlignment(.center)
+                    .padding(.bottom, 56)
+                
+                
+                TextField("Nickname", text: $nickname)
+                    .focused($isTextFieldFocused)
+                    .padding()
+                    .background(.gray.opacity(0.15))
+                    .clipShape(RoundedRectangle(cornerRadius: 16))
+                
+                DSButton(label: "Set nickname", action: {
+                    registerNickname()
+                })
+                .frame(height: 50)
+                
+            }
+            .padding(.horizontal)
+            if isLoading {
+                VStack {
+                    Spacer()
+                    HStack {
+                        Spacer()
+                        ProgressView()
+                        Spacer()
+                    }
+                    Spacer()
+                }
+                .background(.black.opacity(0.4))
+            }
+        }
+        .alert(isPresented: $showAlert) {
+            Alert(
+                title: Text(alertTitle),
+                message: Text(alertSubtitle),
+                dismissButton: .default(Text("Ok"))
+            )
+        }
+    }
+    
+    func showAlert(alertTitle: String, alertSubtitle: String) {
+        self.alertTitle = alertTitle
+        self.alertSubtitle = alertSubtitle
+        showAlert = true
+    }
+    
+    func registerNickname() {
+        isLoading = true
+        if nickname == "" {
+            isLoading = false
+            showAlert(alertTitle: "Empty nickname", alertSubtitle: "You need to fill the nickname field")
+        } else {
+            AuthSingleton.shared.registerInGameNickname(iCloudID: AuthSingleton.shared.actualUser.iCloudID, inGameNickname: nickname) { resultado in
+                isLoading = false
+                DispatchQueue.main.async {
+                    switch resultado {
+                    case .success(let status):
+                        self.handleReturn(status)
+                    case .failure(let erro):
+                        self.handleReturn(erro)
+                    }
+                }
+            }
+        }
+    }
+    
+    func handleReturn(_ status: NickNameStatus) {
+        
+        switch status {
+        case .nicknameInUse:
+            showAlert(alertTitle: "Nickname in use", alertSubtitle: "You need to choose another nickname to your profile")
+        case .userNotFound:
+            showAlert(alertTitle: "User not found", alertSubtitle: "We cant found a user to your credentials")
+        case .serverError(let string):
+            showAlert(alertTitle: "Server error", alertSubtitle: "Try again later or talk with support")
+        case .unknownError:
+            showAlert(alertTitle: "Unknown error", alertSubtitle: "Try again later or talk with support")
+        case .success:
+            AuthSingleton.shared.changeAuthStatus(status: .logged)
+        }
+    }
+}
+
+#Preview {
+    SetNicknameView()
+}
